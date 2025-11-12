760026620dfc32064054b4878ce60efdbb2d0526
ASUS <ASUS@Jiyad-PC>
2025-11-12 10:18:26 +0700

    Pekan 4

---
+++ Praktikum4/src/Pekan4/TextBook.java
@@ -0,0 +1,12 @@
+package Pekan4;
+
+public class TextBook extends Book {
+    private String subject;
+
+    public TextBook(String title, String author, String subject) {
+        super(title, author);
+        this.subject = subject;
+    }
+
+    public String getSubject() { return subject; }
+}
